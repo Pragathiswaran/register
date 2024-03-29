baededda7b5ec528457f8940a0cd107a92491d2a
Pragathiswaran <mpragathiswaran@gmail.com>
2024-03-29 23:27:12 +0530

    commit

--- client/src/components/signin.jsx
+++ client/src/components/signin.jsx
@@ -4,46 +4,71 @@ import { useNavigate } from 'react-router-dom';
 function signin(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [errorUsername, setErrorUsername] = useState('');
+    const [errorPassword, setErrorPassword] = useState('');
+    const [borderUsername, setBorderUsername] = useState('border-dark');
+    const [borderPassword, setBorderPassword] = useState('border-dark');
     const navigate = useNavigate();
 
     const signinHandler = (e) => {
         e.preventDefault();
-        // console.log(username, password);
-        axois.post("http://localhost:3001/login",{username, password})
+        if(!username || !password){
+            if(!username){
+                setErrorUsername('Username is required');
+                setBorderUsername('border-danger');
+            }else{
+                setErrorUsername('');
+                setBorderUsername('border-dark');
+            }
+
+            if(!password){
+                setErrorPassword('Password is required');
+                setBorderPassword('border-danger');
+            }else{
+                setErrorPassword('');
+                setBorderPassword('border-dark');
+            }
+
+        }else{
+            axois.post("http://localhost:3001/login",{username, password})
             .then((res)=>{console.log(res)
                 if(res.data.message === "Success"){
-                    // console.log('Login Successfull');
                     navigate('/dashboard');
                 }
             })
             .catch((err)=>console.error(err));
+        }
     }
     return (
         <>
-        <div className='position-absolute top-50 start-50 translate-middle'>
-        <div className="bg-light border border-dark p-5 rounded-5">
-            <form onSubmit={signinHandler}>
-                <h2 className='mb-4 text-center'>Sign In</h2>
-                <div className="input-group mb-3">
-                    {/* <span class="input-group-text bg-dark text-white" id="inputGroup-sizing-default">Username</span> */}
-                    <input type="text" className="form-control border border-dark" 
-                    aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" 
-                    placeholder='Username' onChange={(e)=>setUsername(e.target.value)}/>
-                </div>
-                <div className="input-group mb-3">
-                    {/* <span class="input-group-text bg-dark text-white" id="inputGroup-sizing-default">password</span> */}
-                    <input type="password" className="form-control border border-dark" aria-label="Sizing example input" 
-                    aria-describedby="inputGroup-sizing-default" placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
-                </div>
-                <div className="d-grid gap-2">
-                    <button className="btn btn-primary" type="submit">Sign In</button>
-                </div>
-            </form>
-            <div className='mt-3'>
-                <p>Don't have an account? <a href="/signup">Signup</a></p>
-            </div>
-        </div>     
-        </div>       
+            <div className='position-absolute top-50 start-50 translate-middle'>
+                <div className="bg-light border border-dark p-5 rounded-5">
+                    <form onSubmit={signinHandler}>
+                        <h2 className='mb-4 text-center'>Sign In</h2>
+                            <div className='mb-3'>
+                                <div className="input-group">
+                                    <input type="text" className={`form-control border ${borderUsername}`} 
+                                    aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" 
+                                    placeholder='Username' onChange={(e)=>setUsername(e.target.value)}/>
+                                </div>
+                                <div className='text-danger'>{errorUsername}</div>
+                            </div>
+                            <div className='mb-3'>
+                            <div className="input-group">
+                                    <input type="password" className={`form-control border ${borderPassword}`} aria-label="Sizing example input" 
+                                    aria-describedby="inputGroup-sizing-default" placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
+                                </div>
+                                <div className='text-danger'>{errorPassword}</div>
+                            </div>
+                            <div className="d-grid gap-2">
+                                <button className="btn btn-primary" type="submit">Sign In</button>
+                            </div>
+                    </form>
+                    <div className='mt-3'>
+                        <p>Don't have an account? <a href="/signup">Signup</a></p>
+                    </div>
+                </div>     
+            </div>       
         </>
     );
 }
--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -7,13 +7,37 @@ function signup(){
     const [email, setEmail] = useState('');
     const [username, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [usernameError, setusernameError] = useState('');
+    const [passwordError, setpasswordError] = useState('');
+    const [errorEmailColor, setEmailErrorColor] = useState('border-dark');
+    const [errorUsernameColor, setUsernameErrorColor] = useState('border-dark');
+    const [errorPasswordColor, setPasswordErrorColor] = useState('border-dark');
     const navigate = useNavigate();
 
     const handler = (e) => {
         e.preventDefault();
         if(!email || !username || !password){
             if(!email){
-
+                setEmailError('Email is required');
+                setEmailErrorColor('border-danger')
+            } else {
+                setEmailError('');
+                setEmailErrorColor('');
+            }
+            if(!username){
+                setusernameError('Username is required');
+                setUsernameErrorColor('border-danger');
+            } else {
+                setusernameError('');
+                setUsernameErrorColor('');
+            }
+            if(!password){
+                setpasswordError('Email is required');
+                setPasswordErrorColor('border-danger');
+            } else {
+                setpasswordError('');
+                setPasswordErrorColor('');
             }
         }else{
             axios.post('http://localhost:3001/register', { email, username, password })
@@ -26,32 +50,37 @@ function signup(){
 
     return (
         <>
-        <div className='position-absolute top-50 start-50 translate-middle'>
-        <div className='bg-primary p-5 rounded-5 bg-light border border-dark'>
-        <form onSubmit={handler}>
+        <div className='position-absolute top-50 start-50 translate-middle position-fixed'>
+        <div className='bg-primary p-5 rounded-5 bg-light border border-dark w-100'>
+        <form className='' onSubmit={handler}>
             <div className='text-center'>
                 <h2 className='text-dark mb-5'>Sign Up</h2>
             </div>
             
-            <div className="input-group mb-4 ">
-                <input type="email" className="form-control border border-dark" 
-                    aria-label="Sizing example input"  
-                    aria-describedby="inputGroup-sizing-default" 
-                    placeholder='Email'
-                    onChange={(e)=>setEmail(e.target.value)}/>
-                    <div className='fs-5'></div>
-            </div>
-            <div className="input-group mb-4">
-                
-                <input type="text" className="form-control border border-dark" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" 
+           <div className="mb-3">
+            <div className="input-group">
+                    <input type="email" className={`form-control border ${errorEmailColor}`} 
+                        aria-label="Sizing example input"  
+                        aria-describedby="inputGroup-sizing-default" 
+                        placeholder='Email'
+                        onChange={(e)=>setEmail(e.target.value)}/>  
+                </div>
+            <div className='text-danger'>{emailError}</div>
+           </div>
+            <div className="mb-3">
+            <div className="input-group">
+                <input type="text" className={`form-control border ${errorUsernameColor}`} aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" 
                 placeholder='Username'  onChange={(e)=>setUserName(e.target.value)}/>
-                <div></div>
             </div>
-            <div className="input-group mb-4">
+            <div className='text-danger'>{usernameError}</div>
+            </div>
+            <div className="mb-4">
+            <div className="input-group ">
                 
-                <input type="password" className="form-control border border-dark" aria-label="Sizing example input" 
+                <input type="password" className={`form-control border ${errorPasswordColor}`} aria-label="Sizing example input" 
                 aria-describedby="inputGroup-sizing-default" placeholder='Password'  onChange={(e)=>setPassword(e.target.value)}/>
-                <div></div>
+            </div>
+            <div className='text-danger'>{passwordError}</div>
             </div>
             <div className="d-grid gap-2">
                 <button className="btn btn-primary" type="submit">Sign up</button>
